d7df730fb78a38abc679cd218c9787800b8fcdd7
k-yomo <kanji.yy@gmail.com>
2023-04-17 21:45:31 +0900

    Update to use google_cloud_run_v2_service

--- dev/google_cloud_run_service.tf
+++ dev/google_cloud_run_service.tf
@@ -1,102 +1,109 @@
-resource "google_cloud_run_service" "invy_api" {
-  name     = "invy-api-${local.env}"
-  location = local.default_region
+resource "google_cloud_run_v2_service" "invy_api" {
+  name         = "invy-api-${local.env}"
+  location     = local.default_region
+  launch_stage = "BETA"
+
   template {
-    spec {
-      containers {
-        image = "asia-northeast1-docker.pkg.dev/invy-prod/invy/invy-api:latest"
-        env {
-          name  = "APP_ENV"
-          value = "dev"
-        }
-        env {
-          name  = "GCP_PROJECT_ID"
-          value = local.project
-        }
-        env {
-          name  = "GCS_AVATAR_IMAGE_BUCKET_NAME"
-          value = google_storage_bucket.avatar_images.name
-        }
-        env {
-          name  = "ALLOWED_ORIGINS"
-          value = "https://invy-app.dev"
-        }
-        env {
-          name  = "DB_NAME"
-          value = google_sql_database.invy.name
-        }
-        env {
-          name  = "DB_USER"
-          value = google_sql_user.postgres.name
-        }
-        env {
-          name = "DB_PASSWORD"
-          value_from {
-            secret_key_ref {
-              name = data.google_secret_manager_secret.invy_db_password.secret_id
-              key  = "latest"
-            }
-          }
-        }
-        env {
-          name  = "DB_CONNECTION_NAME"
-          value = "/cloudsql/${google_sql_database_instance.invy.connection_name}"
-        }
-        env {
-          name  = "REDIS_URL"
-          value = upstash_redis_database.invy.endpoint
-        }
-        env {
-          name  = "REDIS_PASSWORD"
-          value = upstash_redis_database.invy.password
-        }
-        env {
-          name  = "REDIS_PORT"
-          value = upstash_redis_database.invy.port
-        }
-        env {
-          name  = "FIREBASE_SECRET_KEY_PATH"
-          value = "/secrets/firebase/key.firebase.json"
-        }
-        volume_mounts {
-          name       = "firebase"
-          mount_path = "/secrets/firebase"
-        }
+    scaling {
+      min_instance_count = 0
+      max_instance_count = 10
+    }
+    containers {
+      image = "asia-northeast1-docker.pkg.dev/invy-prod/invy/invy-api:latest"
+      env {
+        name  = "APP_ENV"
+        value = "dev"
       }
-      volumes {
-        name = "firebase"
-        secret {
-          secret_name = data.google_secret_manager_secret.firebase_secret_key.secret_id
-          items {
-            key  = "latest"
-            path = "key.firebase.json"
+      env {
+        name  = "GCP_PROJECT_ID"
+        value = local.project
+      }
+      env {
+        name  = "GCS_AVATAR_IMAGE_BUCKET_NAME"
+        value = google_storage_bucket.avatar_images.name
+      }
+      env {
+        name  = "ALLOWED_ORIGINS"
+        value = "https://invy-app.dev"
+      }
+      env {
+        name  = "DB_NAME"
+        value = google_sql_database.invy.name
+      }
+      env {
+        name  = "DB_USER"
+        value = google_sql_user.postgres.name
+      }
+      env {
+        name = "DB_PASSWORD"
+        value_source {
+          secret_key_ref {
+            secret  = data.google_secret_manager_secret.invy_db_password.secret_id
+            version = "latest"
           }
         }
       }
+      env {
+        name  = "DB_CONNECTION_NAME"
+        value = "/cloudsql/${google_sql_database_instance.invy.connection_name}"
+      }
+      env {
+        name  = "REDIS_URL"
+        value = upstash_redis_database.invy.endpoint
+      }
+      env {
+        name  = "REDIS_PASSWORD"
+        value = upstash_redis_database.invy.password
+      }
+      env {
+        name  = "REDIS_PORT"
+        value = upstash_redis_database.invy.port
+      }
+      env {
+        name  = "FIREBASE_SECRET_KEY_PATH"
+        value = "/secrets/firebase/key.firebase.json"
+      }
+      volume_mounts {
+        name       = "cloudsql"
+        mount_path = "/cloudsql"
+      }
+      volume_mounts {
+        name       = "firebase"
+        mount_path = "/secrets/firebase"
+      }
+    }
 
-      service_account_name = google_service_account.invy_api.email
-    }
-    metadata {
-      annotations = {
-        "run.googleapis.com/launch-stage"       = "BETA"
-        "run.googleapis.com/cloudsql-instances" = google_sql_database_instance.invy.connection_name
-        "autoscaling.knative.dev/minScale"      = "0"
-        "autoscaling.knative.dev/maxScale"      = "10"
+    volumes {
+      name = "cloudsql"
+      cloud_sql_instance {
+        instances = [google_sql_database_instance.invy.connection_name]
       }
     }
+
+    volumes {
+      name = "firebase"
+      secret {
+        secret = data.google_secret_manager_secret.firebase_secret_key.secret_id
+        items {
+          mode    = 0
+          path    = "key.firebase.json"
+          version = "latest"
+        }
+      }
+    }
+
+    service_account = google_service_account.invy_api.email
   }
+
   traffic {
-    percent         = 100
-    latest_revision = true
+    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+    percent = 100
   }
-
-  autogenerate_revision_name = true
 
   lifecycle {
     ignore_changes = [
-      metadata.0.annotations,
-      template.0.spec.0.containers.0.image,
-      template.0.metadata.0.annotations,
+      template.0.containers.0.image,
+      annotations,
     ]
   }
 }
@@ -110,22 +117,22 @@ resource "google_cloud_run_domain_mapping" "invy_api" {
   }
 
   spec {
-    route_name = google_cloud_run_service.invy_api.name
+    route_name = google_cloud_run_v2_service.invy_api.name
   }
 }
 
 resource "google_cloud_run_service_iam_member" "all_users_are_run_invoker" {
   project  = local.project
-  location = google_cloud_run_service.invy_api.location
-  service  = google_cloud_run_service.invy_api.name
+  location = google_cloud_run_v2_service.invy_api.location
+  service  = google_cloud_run_v2_service.invy_api.name
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
 
 resource "google_cloud_run_service_iam_member" "api_ci_is_run_admin" {
   project  = local.project
-  location = google_cloud_run_service.invy_api.location
-  service  = google_cloud_run_service.invy_api.name
+  location = google_cloud_run_v2_service.invy_api.location
+  service  = google_cloud_run_v2_service.invy_api.name
   role     = "roles/run.admin"
   member   = "serviceAccount:${google_service_account.api_ci.email}"
 }
